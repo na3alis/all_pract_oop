ed58379cb0973991a834ba9ff2e846438670c072
na3alis <49909358+na3alis@users.noreply.github.com>
2021-01-05 18:38:41 +0200

    pract_1.1

---
+++ practuchna_1/abitutient.h
@@ -0,0 +1,20 @@
+#ifndef ABITUTIENT_H
+#define ABITUTIENT_H
+#include "iostream"
+using namespace std;
+
+class Abitutient
+{
+private:
+    int id;
+    string secomd_name;
+    string name;
+    string fatherly;
+    string adress;
+    string phone_number;
+    float GPA;
+public:
+    Abitutient();
+};
+
+#endif // ABITUTIENT_H

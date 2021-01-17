402efc29549774fab3c88b5946014f5bffe93e7d
na3alis <49909358+na3alis@users.noreply.github.com>
2021-01-17 14:12:47 +0200

    aver_val

---
+++ practuchna_7/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void aver_val(int size)
+{
+    T arr[size];
+    T sum = 0;
+    cout << "Enter the elements of the array" << endl;
+
+    for(int i =0; i < size; i++)
+    {
+        cout << "Enter " << i + 1 << " element of the array" << endl;
+        cin >> arr[i];
+    }
+
+    for(int i =0; i < size; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    for(int i =0; i < size; i++)
+    {
+        if(i == size -1)
+        {
+            cout << arr[i];
+        }
+        else
+        {
+            cout << arr[i] << " + ";
+        }
+    }
+    cout << " = " << sum << endl;
+    cout << "Sum = " << sum << endl;
+
+//    for(int i =0; i < size; i++)
+//    {
+//        cout << i+1 << " element of the array is: " << arr[i] << endl;
+//    }
+};
+
+int main()
+{
+    aver_val<int>(3);
+    aver_val<long>(3);
+    aver_val<double>(3);
+    aver_val<char>(3);
+
+}
